31bd20332b972dd686e785b7ab26e9ce80e9dc9e
SalaniTinko <muhammadarslantoor@gmail.com>
2021-05-03 16:52:18 +0500

    anootation and wildcards routes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,11 +5,25 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
 class QuestionController
 {
     public function homepage()
     {
-        return new Response("Our first oage to understand route / controller and Response ");
+        return new Response("Our first page to understand route / controller and Response ");
+    }
+    /**
+    * @Route("/questions/how-totie-my-shoes-with-magic")
+     */
+    function show()
+    {
+        return new Response("new respince");
+    }
+    /**
+     * @Route("/questions/{slug}")
+     */
+    function show_card($slug)
+    {
+        return new Response(sprintf('new respindddce "%s"!',$slug));
     }
 }
