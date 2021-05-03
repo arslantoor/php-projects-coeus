012f172a57080f90823405708d7aed0a5d70f7df
SalaniTinko <muhammadarslantoor@gmail.com>
2021-05-03 11:40:34 +0500

    initiale symfony setup

--- controler/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../controler/CRUD/database.php";
-$db_obj = new models();
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $session_id= $_GET['id'];
-    $db_obj->logout($session_id);
-}
-?>
--- layout/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <script>document.write(new Date().getFullYear())</script> assignment©Copyright 
-                            </div>
-                    </div>
-                </footer>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
